74975577a47f3e564ee53d9942f9d9b76efe3119
Your Name <you@example.com>
2021-05-06 17:52:15 +0200

    send predicted rom with score for 3rd first class to android client .

--- learning.py
+++ learning.py
@@ -13,9 +13,9 @@ clf = RandomForestClassifier(n_estimators = 100)
 from sklearn import metrics
 
 
-def load2 (data2):
-    extraced_ssid_level_pivot =data2.pivot(columns=['bssid'], values='level')
-    output =pd.concat([data2, extraced_ssid_level_pivot] , axis=1)
+def load(data):
+    extraced_ssid_level_pivot = data.pivot(columns=['bssid'], values='level')
+    output =pd.concat([data, extraced_ssid_level_pivot] , axis=1)
     if 'level' in output:
         del output['level']
     if 'ssid' in output:
@@ -36,30 +36,19 @@ def get_features_and_target(df , target_to_predicte):
 # load data from database in json
 import http.client
 import requests
-r = requests.get('http://127.0.0.1:8091/android/wpa')
+r = requests.get('http://127.0.0.1:8091/android/wap')
 print(r.status_code)
-print (r.json())
 # parse data from json  to dataframe
-df = pd.DataFrame(r.json())
-print(df)
+frame = pd.DataFrame(r.json())
+print(frame)
 # group by date of scan 
-dataset = df.groupby('date').agg('max')
-df2 = load2(dataset)
-df2 = df2.fillna(100)
-print(df2.iloc[:-1])
+#dataset = df.groupby('date').agg('max')
+dataset = load(frame)
+print("dataset before group by date " , dataset)
 
-#  here we can train the model
+dataset['date'] = dataset['date'].astype('float')
 
-from sklearn.neighbors import KNeighborsClassifier
-def learn_and_generate_model() : 
-    X, y =get_features_and_target(dataset, 'salle')
-    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=42)
-    neigh = KNeighborsClassifier(n_neighbors=3)
-    neigh.fit(X_train, np.asarray(y_train, dtype="|S6"))
-    y_pred =  neigh.predict(X_test)
-    # metrics are used to find accuracy or error   
-    print("ACCURACY OF THE MODEL: ", metrics.accuracy_score(np.asarray(y_test, dtype="|S6"), y_pred))
-    from sklearn.metrics import confusion_matrix
-    return neigh , confusion_matrix(np.asarray(y_test, dtype="|S6"), y_pred)
-
-learn_and_generate_model()
+dataset = dataset.groupby('date').agg("max")
+dataset = dataset.fillna(100)
+#dataset = dataset.groupby('date').agg('max')
+print( dataset.iloc[0])
